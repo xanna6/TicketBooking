94d76b75fc523f5983d26191f20c9f487f390736
xanna6 <apiotrow96@gmail.com>
2024-07-25 22:06:22 +0200

    navbar added

--- frontend/ticket_booking/src/App.vue
+++ frontend/ticket_booking/src/App.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="card m-3">
-        <div class="card-body">
-            <div v-if="users">
-                <h5>Users</h5>
-                <ul class="mb-0">
-                    <li v-for="user in users" :key="user.id">{{user.name}}</li>
-                </ul>
-            </div>
-            <div v-if="!users" class="text-center">
-                <div class="spinner-border spinner-border-sm"></div>
-            </div>
-        </div>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand ms-3" href="/">TicketBooking</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline mx-auto my-2 my-lg-0 d-flex">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav ml-auto me-3">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 </template>
 
 <script setup>
